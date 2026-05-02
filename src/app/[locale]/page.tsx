fac265908a13f5f1d05502f0fb9f897490326069
wafflas <giannikakisgiwrgos@gmail.com>
2026-05-02 15:49:57 +0300

    remove bestPicks from homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { setRequestLocale } from "next-intl/server";
 import type { Metadata } from "next";
 import { LandingPage } from "@/components/landing/LandingPage";
-import BestPicks from "@/components/landing/BestPicks";
+//import BestPicks from "@/components/landing/BestPicks";
 import Company from "@/components/landing/Company";
 import ContactSection from "@/components/landing/ContactSection";
 import PartnersSection from "@/components/landing/PartnersSection";
@@ -38,7 +38,7 @@ export default async function LocaleHomePage({
   return (
     <main className="relative flex min-h-[100dvh] w-full min-w-0 flex-col overflow-x-hidden">
       <LandingPage />
-      <BestPicks />
+      {/* <BestPicks /> */}
       <Company />
       <PartnersSection />
       <ContactSection />
